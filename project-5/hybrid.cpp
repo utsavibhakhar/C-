8959fb0fde2fe91c625093bb04823c89fc2409b5
utsavibhakhar <158564889+utsavibhakhar@users.noreply.github.com>
2024-04-29 11:34:24 +0530

    Add files via upload

---
+++ project-5/hybrid.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class A{
+public:
+    int a;
+    void dispa() {
+        cout<<"Enter the number in A = ";
+        cin>>a;   
+    }   
+};
+class B : public A{
+public:
+    int b;
+    void dispb() {
+        cout<<"Enter the number in B = ";
+        cin>>b;   
+    }   
+};
+class C: public A{
+public:
+    int c;
+    void dispc() {
+        cout<<"Enter the number in C = ";
+        cin>>c;   
+    }   
+};
+class D : public B, public C{
+public:
+    int d;
+    void dispd() {
+        cout<<"Enter the number in D = ";
+        cin>>d;   
+    }   
+};
+int main()
+{
+    D d1;
+    int mul=0;
+    
+    d1.B::dispa();
+    d1.dispb();
+    d1.dispc();
+    d1.dispd();
+    
+    mul = d1.B::a * d1.b * d1.c * d1.d;
+    cout<<"Multiplication is: "<<mul<<endl;
+    return 0;
+}
